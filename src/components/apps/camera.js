1570366be0ea31848c8f96c52c72c42ec1047e8a
grawish <grawish06@gmail.com>
2021-07-31 05:44:11 +0530

    camera working properly safari working but unusable

--- src/components/apps/camera.js
+++ src/components/apps/camera.js
@@ -14,15 +14,58 @@ function Camera(props) {
                 });
         }
     }
+    let closeanim = () => {
+        const animdiv = document.querySelector('#animation');
+        const img = document.querySelector('.preview');
+        animdiv.classList.add('no-open');
+        animdiv.classList.remove('open');
+
+        animdiv.style.left = img.offsetLeft+"px";
+        animdiv.style.top = (img.offsetTop+img.parentNode.parentNode.offsetTop)+"px";
+    }
+    let capture = (e) => {
+        let canvas = document.getElementById('canvas-img');
+        let video = document.querySelector('.videoElement');
+        let img = document.querySelector('.preview');
+        let img2 = document.querySelector('#animation');
+        e.target.onclick = function () {
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            canvas.getContext("2d").drawImage(video, 0, 0);
+            // Other browsers will fall back to image/png
+            img2.style.backgroundImage = 'url(' + canvas.toDataURL("image/webp") + ')';
+            img2.classList.remove('no-open');
+            img2.classList.add('open');
+            setTimeout(()=>closeanim(), 100);
+            img.style.backgroundImage = 'url(' + canvas.toDataURL("image/webp") + ')';
+        };
+    }
     useEffect(() => {
         onCamera();
+        closeanim();
     }, [])
     return (
         <div>
-            <video className={'videoElement'} autoPlay={'true'}/>
-            <img src='' />
-            <canvas style={{ display: 'none' }}>
-            </canvas>
+            <div className={'camera-top-controls'}>
+
+            </div>
+            <div id={'animation'} className={'no-open'}/>
+            <video className={'videoElement'} autoPlay={true}/>
+            <div className={'camera-controls'}>
+                <div className={'mode-slider'}>
+
+                </div>
+                <div className={'buttons'}>
+                    <div className={'preview'}>
+                        <canvas className={'pic-canvas'} id={'canvas-img'}>
+
+                        </canvas>
+                    </div>
+                    <div className={'shutter'} onClick={capture}/>
+                    <div className={'switch-cam'}>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/apps/safari.js
+++ src/components/apps/safari.js
@@ -8,12 +8,14 @@ function Safari(props) {
 
             </div>
             <div className={'safari-main'}>
-                <iframe id={'safari'} src={'https://m.google.com/webhp?igu=1'} title={'safari'}>
+                <iframe id={'safari'} src={'https://grawishh.tk'} title={'safari'}>
 
                 </iframe>
             </div>
             <div className={'safari-footer'}>
+                <div>
 
+                </div>
             </div>
         </div>
     );
